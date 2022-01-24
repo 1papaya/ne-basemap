82e968269a352acf5ebd4ff90e4e5db1222fcab3
1papaya <1love1papaya@gmail.com>
2022-01-24 10:27:53 -0700

    use named layers

--- scripts/build-tiles.sh
+++ scripts/build-tiles.sh
@@ -8,6 +8,6 @@ mkdir -p docs/
     --no-tile-compression \
     --output-to-directory="docs/" \
     --maximum-zoom=10 \
-    data/ne_10m_admin_0_boundary_lines_land/ne_10m_admin_0_boundary_lines_land.geojson \
-    data/ne_10m_admin_1_states_provinces_lines/ne_10m_admin_1_states_provinces_lines.geojson \
-    data/ne_10m_populated_places/ne_10m_populated_places.geojson
+    --named-layer=adm0_lines:data/ne_10m_admin_0_boundary_lines_land/ne_10m_admin_0_boundary_lines_land.geojson \
+    --named-layer=adm1_lines:data/ne_10m_admin_1_states_provinces_lines/ne_10m_admin_1_states_provinces_lines.geojson \
+    --named-layer=places:data/ne_10m_populated_places/ne_10m_populated_places.geojson
